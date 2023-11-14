d194af4c72617ade34d0314bb325cbe2bc2fba8f
dudekula meeravali <124290707+meeravali19@users.noreply.github.com>
2023-11-14 17:16:19 +0530

    Create 1704 Determine if string halves are alike day 73.py

---
+++ 1704 Determine if string halves are alike day 73.py
@@ -0,0 +1,47 @@
+"""You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+
+Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+
+Return true if a and b are alike. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "book"
+Output: true
+Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+Example 2:
+
+Input: s = "textbook"
+Output: false
+Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+Notice that the vowel o is counted twice.
+ 
+
+Constraints:
+
+2 <= s.length <= 1000
+s.length is even.
+s consists of uppercase and lowercase letters."""
+
+# program
+
+class Solution(object):
+    def halvesAreAlike(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        c_1=0
+        c_2=0
+        v={'a','e','i','o','u','A','E','I','O','U'}
+        for x in range(len(s)):
+            if x>=(len(s)//2) and s[x] in v:
+                c_2+=1
+            elif s[x] in v:
+                c_1+=1
+        if c_1==c_2:
+            return True
+        return False
+        
